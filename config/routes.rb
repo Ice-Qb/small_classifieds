3a76acd1a4d41a185d8fb0ee4c562d429938332f
Ihor Strumetsky <llighterr@gmail.com>
2013-05-28 05:04:52 +0300

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Try::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   get "users/new"
 
   root to: 'static_pages#home'
   match '/about',   to: 'static_pages#about'
-  match '/signup',  to: 'users#new'
+  
   
 
   # The priority is based upon order of creation:
